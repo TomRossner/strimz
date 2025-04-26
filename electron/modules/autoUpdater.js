505f94052467a666c4c99cda415c5857bafe0f5a
Tom Rossner <tomrossner2@gmail.com>
2025-04-26 13:16:27 +0300

    Fix updateState param

--- electron/modules/autoUpdater.js
+++ electron/modules/autoUpdater.js
@@ -4,7 +4,9 @@ import log from 'electron-log';
 
 const { autoUpdater } = electronUpdater;
 
-export function setupAutoUpdater(win, updateState) {
+export function setupAutoUpdater(win, updateStateParam) {
+  const updateState = updateStateParam || { downloaded: false };
+
   autoUpdater.removeAllListeners();
   
   autoUpdater.on('update-available', async () => {
